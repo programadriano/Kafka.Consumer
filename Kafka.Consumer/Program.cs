69f516e8acdfc5f0aa343500ee3f8816d463e65c
Contato Fluenglish <contato@fluenglish.com.br>
2022-03-20 20:52:56 -0300

    first commit

---
+++ Kafka.Consumer/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Confluent.Kafka;
+using System.Text.Json;
+
+using var c = new ConsumerBuilder<Ignore, string>(new ConsumerConfig
+{
+    GroupId = "test-consumer-group",
+    BootstrapServers = "127.0.0.1:9092",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+}).Build();
+
+{
+    c.Subscribe("test-topic");
+
+    var cts = new CancellationTokenSource();
+    Console.CancelKeyPress += (_, e) =>
+    {
+        e.Cancel = true;
+        cts.Cancel();
+    };
+
+    try
+    {
+        while (true)
+        {
+            try
+            {
+                var cr = c.Consume(cts.Token);
+                Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+
+            }
+            catch (ConsumeException e)
+            {
+                Console.WriteLine($"Error occured: {e.Error.Reason}");
+            }
+        }
+    }
+    catch (OperationCanceledException)
+    {
+        c.Close();
+    }
+}
